7b78eb71cfac50e4ed8f2b7cf8794315f7cd39ea
Luka-Spa <spaninksluka@gmail.com>
2022-04-04 14:04:16 +0200

    Add health check

--- src/api/router/http/router.go
+++ src/api/router/http/router.go
@@ -2,6 +2,7 @@ package httpRouter
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/RealSnowKid/ResIT/config"
 	"github.com/RealSnowKid/ResIT/router/http/handler"
@@ -20,7 +21,12 @@ func Init() {
 		c.File("./static/index.html")
 	})
 	//Routes are defined here
+	api.GET("/health", healthCheck)
 	api.GET("/reservation", handler.GetAllReservations)
 
 	fmt.Println(engine.Run(fmt.Sprintf(":%s", config.GetString("http.port"))))
+}
+
+func healthCheck(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "server is up and running"})
 }
