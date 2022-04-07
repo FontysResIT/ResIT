98751a0e774991a44d512fce68ef6a62abf1eb81
koenrasters <koen.rasters@gmail.com>
2022-04-07 11:13:42 +0200

    fixed issue with primitive.ObjectID in tests

--- src/api/logic/reservation_logic_test.go
+++ src/api/logic/reservation_logic_test.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"testing"
 
 	"github.com/RealSnowKid/ResIT/model"
@@ -22,7 +23,7 @@ func (mock *MockRepository) All() []model.Reservation {
 func TestGetAll(t *testing.T) {
 	mockRepo := new(MockRepository)
 
-	userProfile := model.Reservation{Id: "0", FirstName: "Peter", LastName: "Pancakes", DateTimeSlotId: "0", Email: "peter@example.com", GuestCount: 2, PhoneNumber: "+31 6 12345678"}
+	userProfile := model.Reservation{Id: primitive.ObjectID{}, FirstName: "Peter", LastName: "Pancakes", DateTimeSlotId: "0", Email: "peter@example.com", GuestCount: 2, PhoneNumber: "+31 6 12345678"}
 	// Setup expectations
 	mockRepo.On("All").Return([]model.Reservation{userProfile})
 
