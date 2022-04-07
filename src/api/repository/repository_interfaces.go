121ee86291d867f74946a1911be920eed553105c
i403318 <realsnowkid@gmail.com>
2022-04-07 14:30:14 +0200

    Combine repository interfaces

--- src/api/repository/repository_interfaces.go
+++ src/api/repository/repository_interfaces.go
@@ -2,6 +2,14 @@ package repository
 
 import "github.com/RealSnowKid/ResIT/model"
 
+type IReservation interface {
+	All() []model.Reservation
+}
+
+type ITimeSlot interface {
+	All() []model.TimeSlot
+}
+
 type IDateTimeslot interface {
 	All() []model.DateTimeSlot
 }
--- src/api/repository/timeslot_interface.go
+++
@@ -1,7 +0,0 @@
-package repository
-
-import "github.com/RealSnowKid/ResIT/model"
-
-type ITimeSlot interface {
-	All() []model.TimeSlot
-}
