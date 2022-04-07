4f57325d9d97d05e7140db752f2ecef127e4f58c
Luka-Spa <spaninksluka@gmail.com>
2022-04-07 10:42:21 +0200

    Update config

--- src/api/config/config.go
+++ src/api/config/config.go
@@ -1,7 +1,8 @@
 package config
 
 import (
-	"log"
+	"fmt"
+	"strings"
 
 	"github.com/spf13/viper"
 )
@@ -11,16 +12,20 @@ var config *viper.Viper
 func Init(env string) {
 	var err error
 	config = viper.New()
+	config.SetDefault("http.port", "8080")
 	config.SetConfigType("yaml")
 	config.SetConfigName(env)
 	config.AddConfigPath("../config/")
 	config.AddConfigPath("config/")
+
+	config.AutomaticEnv()
+	config.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
 	err = config.ReadInConfig()
 	if err != nil {
-		log.Fatal("error on parsing configuration file")
+		fmt.Println("Could not parse configuration file, using environment variables")
 	}
 }
-
 
 func GetConfig() *viper.Viper {
 	return config
