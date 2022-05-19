6c87be3e4495d552b1ddda66d4cbcae5bf61528d
i403318 <realsnowkid@gmail.com>
2022-05-19 09:21:47 +0200

    Fix mongo uri

--- src/config_api/repository/mongodb/mongodb.go
+++ src/config_api/repository/mongodb/mongodb.go
@@ -22,7 +22,7 @@ func GetMongoDB() *mongo.Database {
 func Init() {
 	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
 	config := config.GetConfig()
-	connectionString := fmt.Sprintf("%s://%s:%s@%s?retryWrites=true&w=majority", config.GetString("mongo.prefix"), config.GetString("mongo.username"), url.QueryEscape(config.GetString("mongo.password")), config.GetString("mongo.url"))
+	connectionString := fmt.Sprintf("%s://%s:%s@%s/?retryWrites=true&w=majority", config.GetString("mongo.prefix"), config.GetString("mongo.username"), url.QueryEscape(config.GetString("mongo.password")), config.GetString("mongo.url"))
 	clientOptions := options.Client().
 		ApplyURI(connectionString).
 		SetServerAPIOptions(serverAPIOptions)
